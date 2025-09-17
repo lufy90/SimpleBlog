c8ad6acf8cdebd08abf11b75ff8ff05232c61153
Lufei <lufei@uniontech.com>
2025-09-17 19:42:10 +0800

    Add Chinese language.

--- blog/urls.py
+++ blog/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from .admin import admin_site
 
 urlpatterns = [
     path('admin/', admin_site.urls),  # Use custom admin site
     path('accounts/', include('django.contrib.auth.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', include('entries.urls')),
     path('settings/', include('settings.urls')),
 ]
